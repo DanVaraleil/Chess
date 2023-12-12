2e9c899b98a7dafac3e8188a60e6cbe13f0fe4ad
DanVar <danielvaraleil@gmail.com>
2023-12-12 22:14:27 +0300

    Chess 1.0 12.12.2023

---
+++ src/ChessPiece.java
@@ -0,0 +1,16 @@
+abstract public class ChessPiece {
+    String color;
+    boolean check = true;
+    String figure;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+    abstract public String getColor();
+
+    abstract public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
+    abstract public String  getSymbol();
+
+}
